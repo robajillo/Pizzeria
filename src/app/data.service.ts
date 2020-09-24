f39a55d8403a6ba9226bf85a873b2930c1dc2be9
robajillo <rjillo@yahoo.com>
2020-09-24 23:23:51 +0300

    --

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Pizza } from './pizza';
+import { environment } from './../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  
+  pizza: Pizza;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.pizza = new Pizza(0,'','','','','');
+  }
+    pizzaRequest(){
+      interface Response{
+       id: number,
+       flavour: string,
+       size: string,
+       price:string,
+       crust: string,
+       toppings: string
+      }
+      let  promise = new Promise((resolve,reject)=>{
+        this.http.get<Response>(environment.apiUrl).toPromise().then(response=>{
+         this.pizza.crust = response.crust
+         this.pizza.id = response.id
+         this.pizza.flavour = response.flavour
+         this.pizza.size = response.size
+         this.pizza.price = response.price
+         this.pizza.toppings = response.toppings
+         
+        resolve()
+        },
+        error=>{
+          reject(error)
+        
+
+        })
+      })
+      return promise
+    }
 }
--- src/app/pizza.ts
+++ src/app/pizza.ts
@@ -1,11 +1,12 @@
 export class Pizza {
+  
   constructor(
-      public id: number,
-      public flavour: string,
-      public size: string,
-      public price: number,
-      public crust: string,
-      public toppings: string
+       public id: number,
+       public flavour: string,
+       public size: string,
+       public price: string,
+       public crust: string,
+       public toppings: string
 
 
   ){}
