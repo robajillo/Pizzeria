08c8dd31e4f96cb960daad110606cf873777d70a
robajillo <rjillo@yahoo.com>
2020-09-24 12:32:26 +0300

    defined constructors and attributed it to datatype

---
+++ src/app/pizza.ts
@@ -0,0 +1,15 @@
+export class Pizza {
+  constructor(
+      public id: number,
+      public flavour: string,
+      public size: string,
+      public price: number,
+      public crust: string,
+      public toppings: string
+
+          
+      
+
+
+  ){}
+}
